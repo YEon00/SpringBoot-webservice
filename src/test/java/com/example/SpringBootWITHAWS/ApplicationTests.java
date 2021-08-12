cfa1ba142c2902db2af3e5d7b3114fc7d46b5e78
YEon00 <danbicyy@naver.com>
2021-08-12 19:03:34 +0900

    fix java.lang.AssertionError

--- src/test/java/com/example/SpringBootWITHAWS/ApplicationTests.java
+++ src/test/java/com/example/SpringBootWITHAWS/ApplicationTests.java
@@ -5,13 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -23,7 +20,7 @@ class ApplicationTests {
 	@Test
 	public void return_hello() throws Exception{
 
-		String hello = "hello";
+		String hello = "Hello";
 
 		mvc.perform(get("/hello")) // (5)
 				.andExpect(status().isOk()) // (6)

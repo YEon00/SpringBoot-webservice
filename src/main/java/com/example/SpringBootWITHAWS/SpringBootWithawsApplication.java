9fa90f3672175975abb26a5ac024cceeced7ad90
danbicyy <danbicyy@naver.com>
2021-08-12 18:12:16 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/SpringBootWITHAWS/SpringBootWithawsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootWITHAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootWITHAWS/SpringBootWithawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootWITHAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
